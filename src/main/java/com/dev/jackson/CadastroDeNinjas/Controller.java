5494a89733f20e351bab4577e6cc2831e8c468ad
Jacksonflp <jacksonsouza69@icloud.com>
2025-05-20 19:23:11 -0300

    Criado a controller

---
+++ src/main/java/com/dev/jackson/CadastroDeNinjas/Controller.java
@@ -0,0 +1,16 @@
+package com.dev.jackson.CadastroDeNinjas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boasvindas")
+    public String boasVindas(){
+        return "Essa e minha primeira mesagem nessa rota";
+    }
+
+}
